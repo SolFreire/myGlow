80b30f7217bdf182e9b63ff9806bec4e86840e0a
SoraiaFreire <freiresoraia00@gmail.com>
2026-08-31 11:09:49 -0300

    Initial Commit

---
+++ myGlow/myGlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  myGlowApp.swift
+//  myGlow
+//
+//  Created by Soraia Freire Batista on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct myGlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myGlowTests/myGlowTests.swift
@@ -0,0 +1,19 @@
+//
+//  myGlowTests.swift
+//  myGlowTests
+//
+//  Created by Soraia Freire Batista on 31/08/26.
+//
+
+import Testing
+@testable import myGlow
+
+struct myGlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
